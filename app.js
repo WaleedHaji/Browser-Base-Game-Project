f8aabba70d8037b40b9545a839dec565e10bd10a
WaleedHaji <waleed4haji@gmail.com>
2026-08-16 21:03:16 +0300

    feat: added level update and highscore record, added win and lose messages and added a 2 second delay timer for the next round to start when the player wins

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ console.log('Remember the Shapes')
 
 
 /*-------------------------------- Constants --------------------------------*/
+// const winningMessages = [
+//     'Nicely Done!',
+//     'Keep Going!',
+//     'Big Brain Power!',
+//     'Winning!',
+//     'Now we are cooking!'
+// ];
+
 let guessPosition = 0
 
 const shapeSelections = ['🔺', '🟨', '🟢', '🔷']
@@ -28,13 +36,22 @@ message = null
 
 startCountDown = 3
 
+let level = 1
+
+let score = 0
 
 /*------------------------ Cached Element References ------------------------*/
 const allShapeElements = document.querySelectorAll('.shapeBtn')
 
-const startButtonElement = document.querySelector('#startBtn')
+// const startButtonElement = document.querySelector('#startBtn')
 
 const allSequenceBoxEls = document.querySelectorAll('.sqnShapeBox')
+
+const lvlElement = document.querySelector('#stageUpdt')
+
+const highScoreElement = document.querySelector('#stageRecord')
+
+const messageBoxElement = document.querySelector('#messageBox')
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -70,9 +87,28 @@ function displaySequence (){
 
 // displaySequence ()
 
+// ----------------------------------------------------
+// Generate Winning Messages
+
+const winningMessages = [
+    'Nicely Done!',
+    'Keep Going!',
+    'Big Brain Power!',
+    'Winning!',
+    'Now we are cooking!'];
+
+function generateWinningMessage() {
+    const randomIndex = Math.floor(Math.random() * winningMessages.length);
+
+    return winningMessages[randomIndex];
+}
+
 // ------------------------------------------------
 // Start Round Function (calling the Sequence Generator Function & Display Sequence Function)
 function startRound() {
+    allShapeElements.forEach((element)=>{
+        element.disabled = true
+    })
 
     guessPosition = 0;
     guessedSequence = []
@@ -82,6 +118,9 @@ function startRound() {
 
     setTimeout(function() {
         hideSequence();
+            allShapeElements.forEach((element)=>{
+        element.disabled = false
+    })
     }, 3000);
     
 }
@@ -119,9 +158,30 @@ function compare(){
         guessedSequence[2] === sequence[2] &&
         guessedSequence[3] === sequence[3]) {
         console.log("win")
+        
+        level++;
+        lvlElement.textContent = `Level ${level}`;
+
+        score++;
+        highScoreElement.textContent = `Highest ${score}`;
+
+        generateWinningMessage()
+        const winningMessage = generateWinningMessage();
+        messageBoxElement.textContent = winningMessage;
+        setTimeout(function() {
+        startRound();
+            }, 2000);
     }
 
-    else  console.log('incorrect');
+    else {
+    console.log('incorrect');
+
+    messageBoxElement.textContent = 'Sorry, you lost!';
+
+    allShapeElements.forEach((element) => {
+        element.disabled = true;
+    });
+}
 
 }
 
